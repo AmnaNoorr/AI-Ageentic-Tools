a903f2420945d8cbf0afa4126f5a766f14515fee
AmnaNoorr <nooramna0325@gmail.com>
2026-09-22 08:59:48 +0500

    created the req tools and setup agents

---
+++ agent.py
@@ -0,0 +1,12 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.prompts import PromptTemplate 
+from langgraph.prebuilt import create_react_agent
+from tools import tools_list  # Import our tools from tools.py
+
+import warnings
+warnings.filterwarnings("ignore", message="Direct use of automatic function calling")
+
+# 1. FIX: Update the model name to gemini-3.6-flash
+llm = ChatGoogleGenerativeAI(model="gemini-3.6-flash")
+ 
+agent_executor = create_react_agent(llm, tools_list)
---
+++ tools.py
@@ -0,0 +1,21 @@
+from langchain_core.tools import tool
+@tool
+def calculator(expression:string)-> str:
+    #evaluates a basic arithmetic expression
+    try:
+        return(str(eval(expression, {"__builtins__":{}})))
+    except Exception as e:
+        return f"Error: {e}"
+@tool
+def word_length(word: str) -> str:
+    """Returns the number of letters in a word."""
+    return str(len(word))
+
+@tool
+def reverse_string(text: str) -> str:
+    """Reverses the letters in a given text string or word."""
+    return text[::-1]
+
+# Export all tools in a clean list
+tools_list = [calculator, word_length, reverse_string]
+
